65aa35f2fd07d2d2f67232f6899df11053021860
Lucas <lucasrocini@hotmail.com>
2023-04-02 23:12:29 -0300

    Product Image with Multer

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
@@ -7,9 +8,15 @@ import { DetailuserController } from './controllers/user/DetailUserController'
 import { CreateCategoryController } from './controllers/category/CreateCategoryController'
 import { ListCategoryController } from './controllers/category/ListCategoryController'
 
+import { CreateProductController } from './controllers/product/CreateProductController'
+
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
+import uploadConfig from './config/multer'
+
 const router = Router();
+
+const upload = multer(uploadConfig.upload("./tmp"));
 
 //-- ROTAS USER --
 router.post('/users', new CreateUserController().handle)
@@ -22,6 +29,9 @@ router.get('/me', isAuthenticated,  new DetailuserController().handle )
 router.post('/category', isAuthenticated, new CreateCategoryController().handle )
 
 router.get('/category', isAuthenticated, new ListCategoryController().handle )
+
+//-- ROTAS PRODUCT
+router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle )
 
 
 export { router }; 
