281dfe2748f8a5c0793f923a8f0b82a4424392c0
Lucas <lucasrocini@hotmail.com>
2023-04-02 22:23:40 -0300

    User Createion and Authentication

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
-import { Router, Request, Response } from 'express'
+import { Router } from 'express';
+
+import { CreateUserController } from './controllers/user/CreateUserController'
+import { AuthUserController } from './controllers/user/AuthUserController'
 
 const router = Router();
 
-router.get('/teste', (req: Request, res: Response) => {
-    return res.json({nome:"Server Ai!"})
-})
+//-- ROTAS USER --
+router.post('/users', new CreateUserController().handle)
 
-export{ router };
+router.post('/session', new AuthUserController().handle)
+
+export { router }; 
