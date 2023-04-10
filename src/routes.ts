cdae6294fee270a2ff80ea56426047cb7eb7ec1f
Lucas <lucasrocini@hotmail.com>
2023-04-09 23:25:52 -0300

    Create/Delete Order and Add/Remove Items

--- src/routes.ts
+++ src/routes.ts
@@ -11,6 +11,12 @@ import { ListCategoryController } from './controllers/category/ListCategoryContr
 import { CreateProductController } from './controllers/product/CreateProductController'
 import { ListByCategoryController } from './controllers/product/ListByCategoryController'
 
+import { CreateOrderController } from './controllers/order/CreateOrderController'
+import { RemoveOrderController } from './controllers/order/RemoveOrderController'
+
+import { AddItemController } from './controllers/order/AddItemController'
+import { RemoveItemController } from './controllers/order/RemoveItemController'
+
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
 import uploadConfig from './config/multer'
@@ -35,6 +41,13 @@ router.get('/category', isAuthenticated, new ListCategoryController().handle )
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle )
 
 router.get('/category/product', isAuthenticated, new ListByCategoryController().handle )
+
+//-- ROTAS ORDER
+router.post('/order', isAuthenticated, new CreateOrderController().handle )
+router.delete('/order', isAuthenticated, new RemoveOrderController().handle )
+
+router.post('/order/add', isAuthenticated, new AddItemController().handle )
+router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle )
 
 
 export { router }; 
