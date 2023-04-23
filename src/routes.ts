28a97fa229b8be6e3fcefecc224420f8aa884aed
Lucas <lucasrocini@hotmail.com>
2023-04-23 17:45:48 -0300

    Orders - List, Details and Finish

--- src/routes.ts
+++ src/routes.ts
@@ -18,6 +18,10 @@ import { AddItemController } from './controllers/order/AddItemController'
 import { RemoveItemController } from './controllers/order/RemoveItemController'
 import { SendOrderController } from './controllers/order/SendOrderController'
 
+import { ListOrdersController } from './controllers/order/ListOrdersController'
+import { DetailOrderController } from './controllers/order/DetailOrderController'
+import { FinishOrderController } from './controllers/order/FinishOrderController'
+
 
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
@@ -52,6 +56,12 @@ router.post('/order/add', isAuthenticated, new AddItemController().handle )
 router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle )
 
 router.put('/order/send', isAuthenticated, new SendOrderController().handle )
+
+router.get('/orders', isAuthenticated, new ListOrdersController().handle )
+router.get('/order/detail', isAuthenticated, new DetailOrderController().handle )
+
+router.put('/order/finish', isAuthenticated, new FinishOrderController().handle )
+
 
 
 export { router }; 
